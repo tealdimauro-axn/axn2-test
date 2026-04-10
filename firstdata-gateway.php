7bb77830efdfcdd4e9cca429b07e8dbd9da7d7e7
maurotealdi-axn <tealdi.mauro@gmail.com>
2026-04-09 21:14:50 -0300

    nuevo

--- firstdata-gateway.php
+++ firstdata-gateway.php
@@ -9,26 +9,28 @@
   Author URI: https://wanderlust-webdesign.com/
 
   Text Domain: woocommerce-firstdata
-  Domain Path: /languages/
+  Domain Path: 
  */
-add_action( 'before_woocommerce_init', function() {
- 	        if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
- 	                \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
- 	        }
- 	} );
+add_action('before_woocommerce_init', function () {
+    if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    }
+});
 
 add_action('plugins_loaded', 'firstdata_gateway_init', 0);
 
-function firstdata_gateway_init() {
+function firstdata_gateway_init()
+{
 
     if (!class_exists('WC_Payment_Gateway'))
         return;
 
-    include_once( 'firstdata.php' );
+    include_once('firstdata.php');
 
     add_filter('woocommerce_payment_gateways', 'wanderlust_add_firstdata_gateway');
 
-    function wanderlust_add_firstdata_gateway($methods) {
+    function wanderlust_add_firstdata_gateway($methods)
+    {
         $methods[] = 'Wanderlust_Firstdata_Gateway';
         return $methods;
     }
@@ -38,7 +40,8 @@ function firstdata_gateway_init() {
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'wanderlust_firstdata_gateway_action_links');
 
-function wanderlust_firstdata_gateway_action_links($links) {
+function wanderlust_firstdata_gateway_action_links($links)
+{
     $plugin_links = array(
         '<a href="' . admin_url('admin.php?page=wc-settings&tab=checkout&section=wanderlust_firstdata_gateway') . '">' . __('Settings', 'wanderlust-firstdata-gateway') . '</a>',
     );
